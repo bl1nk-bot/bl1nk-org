a9f790edf089cc45d58232b782e8a945dd689af0
billlzzz18 <billlzzz1808@gmail.com>
2026-03-09 00:14:44 +0700

    Create index.ts

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @bl1nk/shared - Shared Types, Constants, and Utilities
+ * Central export point for all shared resources
+ */
+
+export * from "./types";
+export * from "./constants";
+export * from "./utils";
